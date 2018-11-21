fd4d19ca6462756d49cceb6a906a5781bd666d65
Jason Mei <jason.mei403@gmail.com>
2018-11-21 22:14:12 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,13 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.order(rating: :desc).take
   end
+
+  def self.lowest_rating
+    show = self.order(rating: :asc).take
+    show.rating
+  end
+
+  def self.least_popular_show
+    self.order(rating: :asc).take
+  end
 end
