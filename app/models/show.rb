425176476c34a19a605659f7346f801a5d83bb2e
Jason Mei <jason.mei403@gmail.com>
2018-11-21 22:07:44 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ActiveRecord::Base
-  # def highest_rating
-  #   Show.find_by(rating:).order(rating: :desc).take
-  # end
+  def highest_rating
+    Show.find_by(rating:).order(rating: :desc).take
+  end
 end
